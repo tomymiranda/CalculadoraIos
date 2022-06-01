92b3cf77b2bd722284e834dc2f75a0c4007b4030
TomyMiranda <tomasmiranda98@hotmail.com>
2022-06-01 12:50:49 -0300

    se plante base de calculadora y su estilo global

---
+++ calculadoraIos/src/theme/appTheme.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import {StyleSheet} from 'react-native';
+
+// son los estilos GLOBALES de la app
+export const styles = StyleSheet.create({
+  fondo: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  textoCalculadora: {
+    color: 'white',
+    fontSize: 24,
+  },
+});
