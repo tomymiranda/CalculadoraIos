fd0c784dd29a7cc91ffff4ecb90d5fe407ade822
Tomas Miranda <tomasmiranda98@hotmail.com>
2022-06-03 12:19:13 -0300

    se agrega restricciones al momento de agregar numeros

--- calculadoraIos/src/screens/CalculadoraScreen.tsx
+++ calculadoraIos/src/screens/CalculadoraScreen.tsx
@@ -14,8 +14,42 @@ export const CalculadoraScreen = () => {
     setNumero('0');
   }
 
-  const agregarNumero = (numero:string) => {
-    setNumero(numero + numeroAnterior)
+  const agregarNumero = (numeroAAgregar:string) => {
+    //no hay doble punto
+    if(numero.includes('.') && numeroAAgregar === '.'){return;}
+    
+    if(numero.startsWith('0') || numero.startsWith('-0')){
+      //punto decimal
+      if(numeroAAgregar==='.'){
+        setNumero(numero + numeroAAgregar);
+      
+      //evalua si es otro cero y hay un punto
+      }else if(numeroAAgregar==='0' && numero.includes('.')){
+        setNumero(numero + numeroAAgregar);
+      
+      //evalua si es diferente de 0 y no tiene un punto
+      }else if(numeroAAgregar!=='0' && !numero.includes('.')){
+        setNumero(numeroAAgregar);
+      
+      //evita el 000.00
+      }else if(numeroAAgregar==='0' && !numero.includes('.')){
+          setNumero(numero);
+      }else{
+        setNumero(numero + numeroAAgregar);
+      }
+
+    }else{
+      setNumero(numero + numeroAAgregar)
+      
+    }
+  }
+
+  const positivoNegativo = () => {
+    if(numero.includes('-')){
+      setNumero(numero.replace('-',''))
+    }else{
+      setNumero('-' + numero)
+    }
   }
 
   return (
@@ -25,7 +59,7 @@ export const CalculadoraScreen = () => {
 
       <View style={styles.fila}>
         <BotonCalculadora texto="C" color="#9b9b9b" action ={limpiarPantalla}/>
-        <BotonCalculadora texto="+/-" color="#9b9b9b" action ={limpiarPantalla} />
+        <BotonCalculadora texto="+/-" color="#9b9b9b" action ={positivoNegativo} />
         <BotonCalculadora texto="del" color="#9b9b9b" action ={limpiarPantalla} />
         <BotonCalculadora texto="/" color="#ff9427" action ={limpiarPantalla} />
       </View>
