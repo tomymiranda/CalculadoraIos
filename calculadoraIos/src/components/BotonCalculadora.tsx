7f50387bec6a4779b53eb8c7e70fdc4906b8d8ce
Tomas Miranda <tomasmiranda98@hotmail.com>
2022-06-03 11:13:45 -0300

    se agrega funcion de panatalla

--- calculadoraIos/src/components/BotonCalculadora.tsx
+++ calculadoraIos/src/components/BotonCalculadora.tsx
@@ -1,17 +1,25 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {styles} from '../theme/appTheme';
 
 interface Props {
   texto: string;
   color?: string;
+  ancho?: boolean;
+  //no se puede dejar como ? una funcion q se pasa como props porq no se puede pasar como parametro algo q puede llegar como indefinido
+  action: ( numeroTexto: string ) => void;
 }
 
-export const BotonCalculadora = ({texto, color}:Props) => {
+export const BotonCalculadora = ({texto, color = '#2D2D2D', ancho = false, action }:Props) => {
   return (
-    <View style={{... styles.botonCalculadora, backgroundColor:color}}>
-      <Text style={styles.textoBotonCalculadora}>{texto}</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => action(texto)}>
+      <View style={{... styles.botonCalculadora, backgroundColor:color, width: (ancho) ? 180: 80 }}>
+        <Text style={{... styles.textoBotonCalculadora,
+            color:(color==='#9B9B9B')? 'black':'white'}}>{texto}</Text>
+      </View>
+    </TouchableOpacity>
+    
   );
 };
--- calculadoraIos/src/screens/CalculadoraScreen.tsx
+++ calculadoraIos/src/screens/CalculadoraScreen.tsx
@@ -1,22 +1,60 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
+import { useState } from 'react';
 import {Text, View} from 'react-native';
 import { BotonCalculadora } from '../components/BotonCalculadora';
 import { styles } from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
+  
+  const [numeroAnterior, setNumeroAnterior] = useState('');
+  const [numero, setNumero] = useState('0');
+  
+  const limpiarPantalla = () => {
+    setNumero('0');
+  }
+
+  const agregarNumero = (numero:string) => {
+    setNumero(numero + numeroAnterior)
+  }
+
   return (
     <View style={styles.calculadoraContainer}>
-      <Text style={styles.resPequenioCalculadora}>1500.00</Text>
-      <Text style={styles.resCalculadora}>1500.00</Text>
+      <Text style={styles.resPequenioCalculadora}>{numeroAnterior}</Text>
+      <Text style={styles.resCalculadora} numberOfLines={1} adjustsFontSizeToFit >{numero}</Text>
 
       <View style={styles.fila}>
-        <BotonCalculadora texto="C" color="#9b9b9b" />
-        <BotonCalculadora texto="+/-" color="#9b9b9b" />
-        <BotonCalculadora texto="del" color="#9b9b9b" />
-        <BotonCalculadora texto="/" color="#ff9427" />
+        <BotonCalculadora texto="C" color="#9b9b9b" action ={limpiarPantalla}/>
+        <BotonCalculadora texto="+/-" color="#9b9b9b" action ={limpiarPantalla} />
+        <BotonCalculadora texto="del" color="#9b9b9b" action ={limpiarPantalla} />
+        <BotonCalculadora texto="/" color="#ff9427" action ={limpiarPantalla} />
+      </View>
 
-        {/* 2d2d2d gris oscuro  ff9427 naranja*/}
+      <View style={styles.fila}>
+        <BotonCalculadora texto="7"  action = {agregarNumero}/>
+        <BotonCalculadora texto="8"  action = {agregarNumero} />
+        <BotonCalculadora texto="9"  action = {agregarNumero} />
+        <BotonCalculadora texto="X" color="#ff9427" action ={limpiarPantalla}/>
+      </View>
+
+      <View style={styles.fila}>
+        <BotonCalculadora texto="4"  action = {agregarNumero} />
+        <BotonCalculadora texto="5"  action = {agregarNumero} />
+        <BotonCalculadora texto="6"  action = {agregarNumero} />
+        <BotonCalculadora texto="-" color="#ff9427" action ={limpiarPantalla} />
+      </View>
+
+      <View style={styles.fila}>
+        <BotonCalculadora texto="1"  action = {agregarNumero} />
+        <BotonCalculadora texto="2"  action = {agregarNumero} />
+        <BotonCalculadora texto="3"  action = {agregarNumero} />
+        <BotonCalculadora texto="+" color="#ff9427" action ={limpiarPantalla} />
+      </View>
+      
+      <View style={styles.fila}>
+        <BotonCalculadora texto="0" ancho  action = {agregarNumero} />
+        <BotonCalculadora texto="."  action = {agregarNumero} />
+        <BotonCalculadora texto="=" color="#ff9427" action ={limpiarPantalla} />
       </View>
     </View>
   );
