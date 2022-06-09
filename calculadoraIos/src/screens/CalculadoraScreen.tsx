d1c6d906a23edb2315e0d665ea0b322236dea3ea
TomyMiranda <tomasmiranda98@hotmail.com>
2022-06-09 10:37:52 -0300

    se agrega hook useCalculadora para q el codigo sea mas limpio

--- calculadoraIos/src/screens/CalculadoraScreen.tsx
+++ calculadoraIos/src/screens/CalculadoraScreen.tsx
@@ -1,153 +1,80 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { useRef } from 'react';
-import { useState } from 'react';
 import {Text, View} from 'react-native';
 import { BotonCalculadora } from '../components/BotonCalculadora';
 import { styles } from '../theme/appTheme';
-
-enum Operaciones {
-  sumar = '+', restar = '-', multiplicar = '*', dividir = '/',
-}
+import { useCalculadora } from '../hooks/useCalculadora';
 
 export const CalculadoraScreen = () => {
+
   
-  const [numeroAnterior, setNumeroAnterior] = useState('');
-  const [numero, setNumero] = useState('0');
-  const ultOperacion = useRef<Operaciones>()
-  
-  
-  const limpiarPantalla = () => {
-    setNumero('0');
-    setNumeroAnterior('0');
-  }
-
-  const agregarNumero = (numeroAAgregar:string) => {
-    //no hay doble punto
-    if(numero.includes('.') && numeroAAgregar === '.'){return;}
-    
-    if(numero.startsWith('0') || numero.startsWith('-0')){
-      //punto decimal
-      if(numeroAAgregar==='.'){
-        setNumero(numero + numeroAAgregar);
-      
-      //evalua si es otro cero y hay un punto
-      }else if(numeroAAgregar==='0' && numero.includes('.')){
-        setNumero(numero + numeroAAgregar);
-      
-      //evalua si es diferente de 0 y no tiene un punto
-      }else if(numeroAAgregar!=='0' && !numero.includes('.')){
-        setNumero(numeroAAgregar);
-      
-      //evita el 000.00
-      }else if(numeroAAgregar==='0' && !numero.includes('.')){
-          setNumero(numero);
-      }else{
-        setNumero(numero + numeroAAgregar);
-      }
-
-    }else{
-      setNumero(numero + numeroAAgregar)
-      
-    }
-  }
-
-  const positivoNegativo = () => {
-    if(numero.includes('-')){
-      setNumero(numero.replace('-',''))
-    }else{
-      setNumero('-' + numero)
-    }
-  }
-
-  const borrarElUltimoDigito = () => {
-    let negativo = '';
-    let temp = numero;
-
-    if(numero.includes('-')){
-      negativo = '-'
-      temp = numero.substring(1);
-    }
-
-    if(temp.length > 1){
-      setNumero(negativo+temp.slice(0, -1));
-    }else{
-      setNumero('0');
-    }
-  }
-
-  const cambiarNumeroPorElAnterior = () => {
-      
-    if(numero.endsWith('.')){
-      
-      setNumeroAnterior(numero.slice(0, -1));
-    }else{
-      setNumeroAnterior(numero);
-    }
-    setNumero('0');
-  }
-
-  const btnDividir = () => {
-    cambiarNumeroPorElAnterior();
-    ultOperacion.current = Operaciones.dividir;
-  }
-
-  const btnMultiplicar = () => {
-    cambiarNumeroPorElAnterior();
-    ultOperacion.current = Operaciones.multiplicar;
-  }
-
-  const btnRestar = () => {
-    cambiarNumeroPorElAnterior();
-    ultOperacion.current = Operaciones.restar;
-  }
-
-  const btnSumar = () => {
-    cambiarNumeroPorElAnterior();
-    ultOperacion.current = Operaciones.sumar;
-  }
+  //se importa funciones de useCalculadora hook
+  const {
+    numeroAnterior,
+    numero,
+    limpiarPantalla,
+    agregarNumero,
+    positivoNegativo,
+    borrarElUltimoDigito,
+    btnSumar,
+    btnRestar,
+    btnMultiplicar,
+    btnDividir,
+    calcular,
+  } = useCalculadora();
 
   return (
     <View style={styles.calculadoraContainer}>
-      {
-        (numeroAnterior !== '0') && (
-          <Text style={styles.resPequenioCalculadora}>{numeroAnterior}</Text>
-        )
-      }
-      <Text style={styles.resCalculadora} numberOfLines={1} adjustsFontSizeToFit >{numero}</Text>
+      {numeroAnterior !== '0' && (
+        <Text style={styles.resPequenioCalculadora}>{numeroAnterior}</Text>
+      )}
+      <Text
+        style={styles.resCalculadora}
+        numberOfLines={1}
+        adjustsFontSizeToFit>
+        {numero}
+      </Text>
 
       <View style={styles.fila}>
-        <BotonCalculadora texto="C" color="#9b9b9b" action ={limpiarPantalla}/>
-        <BotonCalculadora texto="+/-" color="#9b9b9b" action ={positivoNegativo} />
-        <BotonCalculadora texto="del" color="#9b9b9b" action ={borrarElUltimoDigito} />
-        <BotonCalculadora texto="/" color="#ff9427" action ={btnDividir} />
+        <BotonCalculadora texto="C" color="#9b9b9b" action={limpiarPantalla} />
+        <BotonCalculadora
+          texto="+/-"
+          color="#9b9b9b"
+          action={positivoNegativo}
+        />
+        <BotonCalculadora
+          texto="del"
+          color="#9b9b9b"
+          action={borrarElUltimoDigito}
+        />
+        <BotonCalculadora texto="/" color="#ff9427" action={btnDividir} />
       </View>
 
       <View style={styles.fila}>
-        <BotonCalculadora texto="7"  action = {agregarNumero}/>
-        <BotonCalculadora texto="8"  action = {agregarNumero} />
-        <BotonCalculadora texto="9"  action = {agregarNumero} />
-        <BotonCalculadora texto="X" color="#ff9427" action ={btnMultiplicar}/>
+        <BotonCalculadora texto="7" action={agregarNumero} />
+        <BotonCalculadora texto="8" action={agregarNumero} />
+        <BotonCalculadora texto="9" action={agregarNumero} />
+        <BotonCalculadora texto="X" color="#ff9427" action={btnMultiplicar} />
       </View>
 
       <View style={styles.fila}>
-        <BotonCalculadora texto="4"  action = {agregarNumero} />
-        <BotonCalculadora texto="5"  action = {agregarNumero} />
-        <BotonCalculadora texto="6"  action = {agregarNumero} />
-        <BotonCalculadora texto="-" color="#ff9427" action ={btnRestar} />
+        <BotonCalculadora texto="4" action={agregarNumero} />
+        <BotonCalculadora texto="5" action={agregarNumero} />
+        <BotonCalculadora texto="6" action={agregarNumero} />
+        <BotonCalculadora texto="-" color="#ff9427" action={btnRestar} />
       </View>
 
       <View style={styles.fila}>
-        <BotonCalculadora texto="1"  action = {agregarNumero} />
-        <BotonCalculadora texto="2"  action = {agregarNumero} />
-        <BotonCalculadora texto="3"  action = {agregarNumero} />
-        <BotonCalculadora texto="+" color="#ff9427" action ={btnSumar} />
+        <BotonCalculadora texto="1" action={agregarNumero} />
+        <BotonCalculadora texto="2" action={agregarNumero} />
+        <BotonCalculadora texto="3" action={agregarNumero} />
+        <BotonCalculadora texto="+" color="#ff9427" action={btnSumar} />
       </View>
-      
+
       <View style={styles.fila}>
-        <BotonCalculadora texto="0" ancho  action = {agregarNumero} />
-        <BotonCalculadora texto="."  action = {agregarNumero} />
-        <BotonCalculadora texto="=" color="#ff9427" action ={limpiarPantalla} />
+        <BotonCalculadora texto="0" ancho action={agregarNumero} />
+        <BotonCalculadora texto="." action={agregarNumero} />
+        <BotonCalculadora texto="=" color="#ff9427" action={calcular} />
       </View>
     </View>
   );
